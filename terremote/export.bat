94261f2b13bb4b98e7ab99d53368698ac8a1d06e
Nick2253 <nick2253@gmail.com>
2016-06-14 10:17:43 -0700

    Update export.bat

--- terremote/export.bat
+++ terremote/export.bat
@@ -1,2 +1,2 @@
 del export.ter
-"c:\program files\7-zip\7z.exe" a -tzip export.ter * -x!export.bat -x!README.md
+"c:\program files\7-zip\7z.exe" a -tzip export.ter * -x!export.bat -x!README.md -x!schema.json
