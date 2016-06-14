5d7323ee4d589723f751f1c7a6c55db9b44fe77c
Nick2253 <nick2253@users.noreply.github.com>
2016-06-14 10:17:35 -0700

    Add Android Push to Bat

--- terremote/export.bat
+++ terremote/export.bat
@@ -1,2 +1,3 @@
 del export.ter
 "c:\program files\7-zip\7z.exe" a -tzip export.ter * -x!export.bat -x!README.md
+"C:\program files (x86)\Android\android-sdk\platform-tools\adb" push export.ter /sdcard/
