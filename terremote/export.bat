753b1094b6636233e60d2b3fe17a30ce1b5af7d5
Nick2253 <nick2253@gmail.com>
2016-06-14 10:27:15 -0700

    Update export.bat

--- terremote/export.bat
+++ terremote/export.bat
@@ -1,7 +1,3 @@
 del export.ter
-<<<<<<< HEAD
-"c:\program files\7-zip\7z.exe" a -tzip export.ter * -x!export.bat -x!README.md
-"C:\program files (x86)\Android\android-sdk\platform-tools\adb" push export.ter /sdcard/
-=======
 "c:\program files\7-zip\7z.exe" a -tzip export.ter * -x!export.bat -x!README.md -x!schema.json
->>>>>>> 94261f2b13bb4b98e7ab99d53368698ac8a1d06e
+"C:\program files (x86)\Android\android-sdk\platform-tools\adb" push export.ter /sdcard/
